39b357cd8fa46f78eb471cb2c4030b86a00ca236
Mijeong Ban <mban1@stevens.edu>
2020-02-11 17:24:33 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HalfundecidedRouter from './pages/HalfundecidedRouter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <HalfundecidedRouter />
+        </>
+    );
+};
 
 export default App;

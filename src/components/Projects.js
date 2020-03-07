5669d33f4b91406a11cfac979ec35d168d626efd
Mijeong Ban <mban1@stevens.edu>
2020-03-07 11:53:29 -0500

    projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 import { makeStyles, Grid, Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, Avatar, IconButton, Typography } from '@material-ui/core';
+import '../assets/styles/index.css';
 import clsx from 'clsx';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import fractionWeb from '../assets/images/fractionweb.png';
+import fractionApp from '../assets/images/fractionapp.png';
+import consiliumq from '../assets/images/consiliumq.png';
+import mood from '../assets/images/mood.png';
+import rateit from '../assets/images/rateit.png';
+import sharewithme from '../assets/images/sharewithme.png';
+import dinnerspinner from '../assets/images/dinnerspinner.png';
+import educoin from '../assets/images/educoin.png';
+import movieapp from '../assets/images/movieapp.png';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -12,7 +21,7 @@ const useStyles = makeStyles(theme => ({
     media: {
         height: 0,
         paddingTop: '90%', // 16:9
-        filter: 'brightness(70%)',
+        filter: 'brightness(80%)',
     },
     expand: {
         transform: 'rotate(0deg)',
@@ -35,7 +44,7 @@ const projectData = [
         title: 'Fraction Web Service',
         period: 'Currently Working',
         image: fractionWeb,
-        body: 'input1',
+        body: 'React, AWS Lambda, API Gateway, Cognito, Amplify, DynamoDB',
         reference: 'https://fraction.rent/',
         detail: 'detail1',
     },
@@ -44,8 +53,8 @@ const projectData = [
         code: 'fraction2',
         title: 'Fraction App',
         period: 'Currently Working',
-        image: '',
-        body: 'input2',
+        image: fractionApp,
+        body: 'React Native, AWS Lambda, API Gateway, Cognito, Amplify, DynamoDB',
         reference: '',
         detail: 'detail1',
     },
@@ -53,70 +62,70 @@ const projectData = [
         avatar: 'C',
         code: 'consiliumq',
         title: 'ConsiliumQ',
-        period: 'Currently Working',
-        image: '',
-        body: 'input1',
-        reference: '',
+        period: 'September 2019 - December 2019',
+        image: consiliumq,
+        body: 'React, D3.js, TypeScript, GraphQL, MongoDB, AWS',
+        reference: 'https://github.com/consiliumQ',
         detail: 'detail1',
     },
     {
         avatar: 'M',
         code: 'mood',
         title: 'Mood',
-        period: 'Currently Working',
-        image: '',
-        body: 'input1',
-        reference: '',
+        period: 'March 2019 - May 2019',
+        image: mood,
+        body: 'HTML5, CSS3, JavaScript, Handlebars, Express, MongoDB',
+        reference: 'https://github.com/group1forpatrick/mood',
         detail: 'detail1',
     },
     {
         avatar: 'R',
         code: 'rateit',
         title: 'RateIt',
-        period: 'Currently Working',
-        image: '',
-        body: 'input1',
-        reference: '',
+        period: 'March 2019 - May 2019',
+        image: rateit,
+        body: 'HTML5, CSS3, JavaScript, Handlebars, Express, MongoDB',
+        reference: 'https://github.com/halfundecided/rateit-website',
         detail: 'detail1',
     },
     {
         avatar: 'S',
         code: 'sharewithme',
         title: 'Share With Me',
-        period: 'Currently Working',
-        image: '',
-        body: 'input1',
-        reference: '',
+        period: 'February 2019',
+        image: sharewithme,
+        body: 'React, GraphQL, Apollo Client',
+        reference: 'https://github.com/halfundecided/sharewithme-react-graphql',
         detail: 'detail1',
     },
     {
         avatar: 'M',
         code: 'movieapp',
         title: 'Movie App',
-        period: 'Currently Working',
-        image: '',
-        body: 'input1',
-        reference: '',
+        period: 'January 2019',
+        image: movieapp,
+        body: 'React',
+        reference: 'https://github.com/halfundecided/movie-web-react',
         detail: 'detail1',
     },
     {
         avatar: 'E',
         code: 'educoin',
         title: 'EduCoin',
-        period: 'Currently Working',
-        image: '',
-        body: 'input1',
-        reference: '',
+        period: 'January 2018 - May 2018',
+        image: educoin,
+        body: 'React Native, WordPress, Python, PHP, HTML5',
+        reference: 'https://github.com/halfundecided/cryptocurrency_EduCoin',
         detail: 'detail1',
     },
     {
         avatar: 'D',
         code: 'DinnerSpinner',
         title: 'Movie App',
-        period: 'Currently Working',
-        image: '',
-        body: 'input1',
-        reference: '',
+        period: 'September 2017 - December 2017',
+        image: dinnerspinner,
+        body: 'HTML5, CSS3, JavaScript',
+        reference: 'http://www.mijeong.me/CS146_DinnerSpinner/',
         detail: 'detail1',
     },
 ];
@@ -130,7 +139,7 @@ const Projects = () => {
     };
 
     return (
-        <Grid container spacing={4} className="project">
+        <Grid container spacing={4}>
             {projectData.map(project => (
                 <Grid item md={6} sm={6} xs={12}>
                     <Card className={classes.root} variant="outlined">

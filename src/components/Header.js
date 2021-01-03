bafe26c6a942955a5d379159ba55f9223f442aa6
Mijeong Ban <mban1@stevens.edu>
2021-01-03 11:57:45 -0500

    title changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <div>
       <PageHeader
         className="header"
-        title={`Mijeong's Space`}
+        title={` `}
         // subTitle={`welcome!`}
         avatar={{
           size: { xs: 40, sm: 86, md: 128, lg: 128, xl: 128, xxl: 128 },
